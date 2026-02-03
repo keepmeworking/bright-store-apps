2c0d4226ddeee59fcbd4141ae1d971acf6fbc952
keepmespace <keepmespace@gmail.com>
2026-02-03 19:24:19 +0000

    feat: add dummy-payment app and file-based config for stripe

--- apps/stripe/src/modules/app-config/repositories/app-config-repo-impl.ts
+++ apps/stripe/src/modules/app-config/repositories/app-config-repo-impl.ts
@@ -1,6 +1,14 @@
-import { DynamodbAppConfigRepo } from "@/modules/app-config/repositories/dynamodb/dynamodb-app-config-repo";
+import path from "path";
+
+import { env } from "@/lib/env";
+
+import { DynamodbAppConfigRepo } from "./dynamodb/dynamodb-app-config-repo";
+import { FileAppConfigRepo } from "./file/file-app-config-repo";
 
 /*
  * Replace this implementation with custom DB (Redis, Metadata etc) to drop DynamoDB and bring something else
  */
-export const appConfigRepoImpl = new DynamodbAppConfigRepo();
+export const appConfigRepoImpl =
+  env.APL === "file"
+    ? new FileAppConfigRepo(path.join(process.cwd(), ".data", "stripe-config.json"))
+    : new DynamodbAppConfigRepo();
