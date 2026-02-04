8fd603eea4ed04344e951884d693bb1f6a2aa6e6
keepmespace <keepmespace@gmail.com>
2026-02-04 03:41:24 +0000

    fix(dummy-payment): move outputFileTracingRoot to root of nextConfig

--- apps/dummy-payment/next.config.js
+++ apps/dummy-payment/next.config.js
@@ -11,9 +11,9 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  outputFileTracingRoot: path.join(__dirname, "../../"),
   reactStrictMode: true,
   experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../../"),
     optimizePackageImports: [
       "@sentry/nextjs",
       "@sentry/node",
