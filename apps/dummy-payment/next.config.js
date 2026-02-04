fdabf6aa08fdaaaa528ff4ef5cff37bcc3ff0752
keepmespace <keepmespace@gmail.com>
2026-02-04 03:30:28 +0000

    fix(dummy-payment): enable standalone output for docker build

--- apps/dummy-payment/next.config.js
+++ apps/dummy-payment/next.config.js
@@ -23,6 +23,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: "standalone",
   /*
    * Ignore opentelemetry warnings - https://github.com/open-telemetry/opentelemetry-js/issues/4173
    * Remove when https://github.com/open-telemetry/opentelemetry-js/pull/4660 is released
