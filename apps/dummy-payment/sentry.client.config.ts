ea230497aa4268a562d98814cf7398ee4a7f2274
keepmespace <keepmespace@gmail.com>
2026-02-04 10:48:36 +0000

    fix(dummy-payment): fix sentry config and standardize package.json to resolve build errors

--- apps/dummy-payment/sentry.client.config.ts
+++ apps/dummy-payment/sentry.client.config.ts
@@ -8,7 +8,6 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  enableTracing: false,
   environment: process.env.ENV,
   includeLocalVariables: true,
   ignoreErrors: ["TRPCClientError"],
